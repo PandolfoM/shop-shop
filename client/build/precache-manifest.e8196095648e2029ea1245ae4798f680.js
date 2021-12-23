98f4694b65ab6f9c8178003377122daa6482664d
Matthew Pandolfo <matt@pandolfo.com>
2021-12-23 14:39:15 -0500

    stripe integration

---
+++ client/build/precache-manifest.e8196095648e2029ea1245ae4798f680.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f848e27b4b8e0a418bbfc94c421c04a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52161bbaaebc1a3facd1",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "4f32bb981a8f8bcf1673",
+    "url": "/static/js/2.f8ec6b26.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.f8ec6b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52161bbaaebc1a3facd1",
+    "url": "/static/js/main.54d8162b.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
