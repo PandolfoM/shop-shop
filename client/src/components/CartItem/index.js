4d912a7f182023dbe2751e0a904c4e78bc07bab0
Matthew Pandolfo <matt@pandolfo.com>
2021-12-23 12:14:38 -0500

    offline functionality

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
   const [, dispatch] = useStoreContext();
-
-  const removeFromCart = (item) => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id,
-    });
-  };
 
   const onChange = (e) => {
     const value = e.target.value;
@@ -20,13 +14,23 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id,
       });
+      idbPromise("cart", "delete", { ...item });
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value),
       });
+      idbPromise("cart", "put", { ...item, purchaseQuantity: parseInt(value) });
     }
+  };
+
+  const removeFromCart = (item) => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+    });
+    idbPromise("cart", "delete", { ...item });
   };
 
   return (
